29a77bc8e549012c97bd1160271e0bc628a02645
Petr Mrázek <peterix@gmail.com>
2017-03-01 01:00:14 +0100

    Update 2017-03-01

--- metautil.py
+++ metautil.py
@@ -1,7 +1,35 @@
 import json
 from pprint import pprint
+from jsonobject import *
 
-# TODO: use https://github.com/dimagi/jsonobject
+'''
+{
+    "latest": {
+        "release": "1.11.2",
+        "snapshot": "17w06a"
+    },
+    "versions": [
+        {
+            "id": "17w06a",
+            "releaseTime": "2017-02-08T13:16:29+00:00",
+            "time": "2017-02-08T13:17:20+00:00",
+            "type": "snapshot",
+            "url": "https://launchermeta.mojang.com/mc/game/7db0c61afa278d016cf1dae2fba0146edfbf2f8e/17w06a.json"
+        }
+    ]
+}
+'''
+
+class MojangIndexEntry(JsonObject):
+    id = StringProperty()
+    releaseTime = ISOTimestampProperty()
+    time = ISOTimestampProperty()
+    type = StringProperty()
+    url = StringProperty()
+
+class MojangIndex(JsonObject):
+    latest = DictProperty(StringProperty)
+    versions = ListProperty(MojangIndexEntry)
 
 class GradleSpecifier:
     'A gradle specifier - a maven coordinate'
