bf231ce514a7a5852fb493b0f77dec8909bf3502
Petr Mrázek <peterix@gmail.com>
2017-04-14 22:10:07 +0200

    Add an easy way to switch between meta develop and production

--- config.sh
+++ config.sh
@@ -2,4 +2,7 @@ export UPSTREAM_DIR=upstream
 export UPSTREAM_REPO=git@github.com:MultiMC/meta-upstream.git
 export MMC_DIR=multimc
 export MMC_REPO=git@github.com:MultiMC/meta-multimc.git
-
+export S3_master=s3://meta.multimc.org
+export S3_develop=s3://meta-dev.multimc.org
+export BRANCH_master=master
+export BRANCH_develop=develop
--- update.sh
+++ update.sh
@@ -7,6 +7,17 @@ BASEDIR=`pwd`
 set -x
 
 source config.sh
+if [ -f config_local.sh ]; then
+    source config_local.sh
+fi
+
+MODE=${MODE:-develop}
+
+S3_BUCKET_var="S3_$MODE"
+S3_BUCKET="${!S3_BUCKET_var}"
+
+BRANCH_var="BRANCH_$MODE"
+BRANCH="${!BRANCH_var}"
 
 function fail_in {
     cd "${BASEDIR}/${UPSTREAM_DIR}"
@@ -22,15 +33,26 @@ function fail_out {
 
 currentDate=`date --iso-8601`
 
+cd "${BASEDIR}/${UPSTREAM_DIR}"
+git reset --hard HEAD || exit 1
+git checkout ${BRANCH} || exit 1
+cd "${BASEDIR}"
+
 ./updateMojang.py || fail_in
 ./updateForge.py || fail_in
 ./updateLiteloader.py || fail_in
+
 cd "${BASEDIR}/${UPSTREAM_DIR}"
 git add mojang/version_manifest.json mojang/versions/* mojang/assets/* forge/*.json liteloader/*.json || fail_in
 if ! git diff --cached --exit-code ; then
     git commit -a -m "Update ${currentDate}" || fail_in
     git push || exit 1
 fi
+cd "${BASEDIR}"
+
+cd "${BASEDIR}/${MMC_DIR}"
+git reset --hard HEAD || exit 1
+git checkout ${BRANCH} || exit 1
 cd "${BASEDIR}"
 
 ./generateMojang.py || fail_out
@@ -46,6 +68,6 @@ if ! git diff --cached --exit-code ; then
 fi
 cd "${BASEDIR}"
 
-s3cmd --exclude=".git*" --delete-removed sync ${BASEDIR}/${MMC_DIR}/ s3://meta.multimc.org || exit 2
+s3cmd --exclude=".git*" --delete-removed sync ${BASEDIR}/${MMC_DIR}/ ${S3_BUCKET} || exit 2
 
 exit 0
