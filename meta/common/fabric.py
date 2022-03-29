6d9561c28dbabeec600ee647f5a4f5870f78d523
Sefa Eyeoglu <contact@scrumplex.net>
2022-03-29 13:36:29 +0200

    fix: use correct directory for fabric meta

--- meta/common/fabric.py
+++ meta/common/fabric.py
@@ -4,7 +4,7 @@ BASE_DIR = "fabric"
 
 JARS_DIR = join(BASE_DIR, "jars")
 INSTALLER_INFO_DIR = join(BASE_DIR, "loader-installer-json")
-META_DIR = join(BASE_DIR, "loader-installer-json")
+META_DIR = join(BASE_DIR, "meta-v2")
 
 LOADER_COMPONENT = "net.fabricmc.fabric-loader"
 INTERMEDIARY_COMPONENT = "net.fabricmc.intermediary"
