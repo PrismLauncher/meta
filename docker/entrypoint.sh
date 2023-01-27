b632612e7dca2cd901f6c39ccc7501e263f4ef2b
Sefa Eyeoglu <contact@scrumplex.net>
2023-01-27 18:45:49 +0100

    fix: update cron command for cronie
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" == "update" ]; then
     cd /app || exit 1
     exec su user -c "bash ./update.sh"
 elif [ "$1" == "cron" ]; then
-    exec cron -f
+    exec crond -f
 else
     exec "$@"
 fi
