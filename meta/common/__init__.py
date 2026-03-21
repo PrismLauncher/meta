d55bc04feeb79eb7a8febe681bfad8eca27aac6f
Trial97 <alexandru.tripon97@gmail.com>
2026-03-21 11:49:52 +0200

    fix error
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- meta/common/__init__.py
+++ meta/common/__init__.py
@@ -117,7 +117,7 @@ def get_file_sha1_from_file(file_name: str, sha1_file: str) -> Optional[str]:
     if os.path.isfile(sha1_file):
         with open(sha1_file, "r") as file:
             return file.read()
-    if not os.path.isfile(jar_path):
+    if not os.path.isfile(file_name):
         return None
     new_sha1 = file_hash(file_name, hashlib.sha1)
     with open(sha1_file, "w") as file:
