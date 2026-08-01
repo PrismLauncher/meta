a566638ce3a83c3f55443145997bd23359791072
Octol1ttle <l1ttleofficial@outlook.com>
2026-08-01 18:21:39 +0500

    chore: bump ForgeWrapper to prism-2026-08-01

--- meta/common/forge.py
+++ meta/common/forge.py
@@ -15,8 +15,8 @@ LEGACYINFO_FILE = join(BASE_DIR, "legacyinfo.json")
 FORGE_COMPONENT = "net.minecraftforge"
 
 FORGEWRAPPER_LIBRARY = make_launcher_library(
-    GradleSpecifier("io.github.zekerzhayard", "ForgeWrapper", "prism-2025-12-07"),
-    "4c4653d80409e7e968d3e3209196ffae778b7b4e",
-    29731,
+    GradleSpecifier("io.github.zekerzhayard", "ForgeWrapper", "prism-2026-08-01"),
+    "852b7e59748da1512d40e38407eadb1f0031a996",
+    29800,
 )
 BAD_VERSIONS = ["1.12.2-14.23.5.2851"]
